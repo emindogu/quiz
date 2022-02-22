53f02c27c7d4e1aa444c47a796a10a26def8ac64
emindogu <emindogu@hotmail.com>
2022-02-22 12:44:04 +0300

    sınav sayfası oluşturuldu.

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -5,17 +5,49 @@
 
     <div class="card">
         <div class="card-body">
-            <p class="card-text">
-                @foreach ($collection as $item)
+            <form method="POST" action="">
+                @foreach ($quiz->questions as $question)
+                <strong>{{$loop->iteration}})</strong> {{$question->question}}
+
+                @if ($question->image)
+                <img src="{{asset($question->image)}}" style="width:200px" alt="" class="img-responsive">
+                @endif
+
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
-                        value="option1" checked>
-                    <label class="form-check-label" for="exampleRadios1">
-                        Default radio
+                    <input class="form-check-input" type="radio" name="{{$question->id}}" id="quiz{{$question->id}}1"
+                        value="answer1" required>
+                    <label class="form-check-label" for="quiz{{$question->id}}1">
+                        {{$question->answer1}}
                     </label>
                 </div>
-                 @endforeach
-            </p>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="{{$question->id}}" id="quiz{{$question->id}}2"
+                        value="answer2" required>
+                    <label class="form-check-label" for="quiz{{$question->id}}2">
+                        {{$question->answer2}}
+                    </label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="{{$question->id}}" id="quiz{{$question->id}}3"
+                        value="answer3" required>
+                    <label class="form-check-label" for="quiz{{$question->id}}3">
+                        {{$question->answer3}}
+                    </label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="{{$question->id}}" id="quiz{{$question->id}}4"
+                        value="answer4" required>
+                    <label class="form-check-label" for="quiz{{$question->id}}4">
+                        {{$question->answer4}}
+                    </label>
+                </div>               
+                <hr>               
+                @endforeach
+                <button type="submit" class="btn btn-success">Sınavı Bitir</button>
+            </form>
         </div>
     </div>
 
